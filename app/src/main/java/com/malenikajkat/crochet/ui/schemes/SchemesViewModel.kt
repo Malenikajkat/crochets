e8a2a4ff806aa249312930f81370718ae9e3a35d
malenikajkat <malenikajkat@mail.ru>
2024-06-30 18:35:51 +0300

    Initial commit

---
+++ app/src/main/java/com/malenikajkat/crochet/ui/schemes/SchemesViewModel.kt
@@ -0,0 +1,13 @@
+package com.malenikajkat.crochet.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
